b0fd05752c302a458c9d207586718b2b70321972
JakubSyperek <162420@student.uwm.edu.pl>
2022-02-14 14:51:04 +0100

    h

--- src/pl/edu/uwm/wmii/syperekjakub/laboratorium00/Z1/Klasa.java
+++ src/pl/edu/uwm/wmii/syperekjakub/laboratorium00/Z1/Klasa.java
@@ -3,8 +3,10 @@ package pl.edu.uwm.wmii.syperekjakub.laboratorium00.Z1;
 public class Klasa {
 
 
-    private Object jestPalindromem = null;
+    public Object jestPalindromem;
 
+    static {
+    }
 
     public Klasa(Object jestPalindromem) {
         this.jestPalindromem = jestPalindromem;
--- src/pl/edu/uwm/wmii/syperekjakub/laboratorium00/Z3/Z3.java
+++ src/pl/edu/uwm/wmii/syperekjakub/laboratorium00/Z3/Z3.java
@@ -4,4 +4,8 @@ package pl.edu.uwm.wmii.syperekjakub.laboratorium00.Z3;
 public class Z3 {
     void FileFilter(){}
 
+    public static void zwrocPodkatalogi(){
+
+    }
+
 }
