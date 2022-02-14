82abbbd4c61eac3834087a14d0895d404e5240a2
JakubSyperek <162420@student.uwm.edu.pl>
2022-02-14 14:21:10 +0100

    Initial commit

---
+++ src/pl/edu/uwm/wmii/syperekjakub/laboratorium00/Z1/Klasa.java
@@ -0,0 +1,21 @@
+package pl.edu.uwm.wmii.syperekjakub.laboratorium00.Z1;
+
+public class Klasa {
+
+
+    private Object jestPalindromem = null;
+
+
+    public Klasa(Object jestPalindromem) {
+        this.jestPalindromem = jestPalindromem;
+    }
+
+    interface Comparable{
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/pl/edu/uwm/wmii/syperekjakub/laboratorium00/Z2/Z2.java
@@ -0,0 +1,13 @@
+package pl.edu.uwm.wmii.syperekjakub.laboratorium00.Z2;
+
+import java.util.Iterator;
+
+public class Z2 {
+    public static int[] x = {1,2,3};
+
+    public static <E> void wypiszCoDrugi(){
+
+
+
+    }
+}
